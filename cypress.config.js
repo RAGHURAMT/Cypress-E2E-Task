3cde28e287a8d5d54319b2f40b27dd837c6c50f9
Raghu <admin@Admins-MacBook-Pro.local>
2024-06-19 10:57:48 +0100

    setting up the project

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    baseUrl:'https://www.saucedemo.com/',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('file:preprocessor', cucumber())
+    },
+    specPattern:"cypress/e2e/features/*.feature",
+  },
+});
